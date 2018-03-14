6166169b4fde24a2df52f2bd50bc08a9255cf092
ook-yoga <hugo.p.cmu@gmail.com>
2018-03-14 12:18:42 -0700

    template script for running jobs

---
+++ scripts/template.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Navigate to proper directory and activate the conda environment
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/..
+source activate gym
+
+# Log directory is based on run date
+DATE=$(date +%Y-%m-%d)
+
+# Run several different training runs, storing in different log locations
+NAME="experiment_name"
+python train.py --log=$DATE$NAME \
+--num_train_cycles=1\
+--buffer_size=1\
+--num_trajectories=1\
+--num_learning_iterations=1\
+--episode_batch_size=1
